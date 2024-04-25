53061304a32ce16bb27fb768b163a73c2eefe154
Vashisth Tiwari <vashistt@lovelace.ece.local.cmu.edu>
2024-04-25 16:29:34 -0400

    messages

--- lora_ft/Run_evals.py
+++ lora_ft/Run_evals.py
@@ -653,11 +653,14 @@ def main():
         # out_str = "[SpeedUp={:.3f}] Original perplexity on wikitext = {:.3f} | Before Training perplexity on wikitext = {:.3f}".format(speedup, og_ppl, before_train_ppl, speedup)
         # out_file.write(out_str + "\n")
         # print(out_str)
-    adapter_name = '/home/vashistt/Desktop/anlp-project/finetuned_model_prune_c4_ft_wiki/'
-    # model.load_adapter(adapter_name)
-    # model.set_active_adapters('prune-c4_ft_wiki_adapter')
+        finetuned_model = False
+    if finetuned_model:
+        
+        adapter_name = '/home/vashistt/Desktop/anlp-project/finetuned_model_prune_c4_ft_wiki/'
+        # model.load_adapter(adapter_name)
+        # model.set_active_adapters('prune-c4_ft_wiki_adapter')
 
-    model = PeftModel.from_pretrained(model, adapter_name, adapter_name="prune_c4_ft_wiki_adapter")
+        model = PeftModel.from_pretrained(model, adapter_name, adapter_name="prune_c4_ft_wiki_adapter")
 
 
     if should_i_do_eleuther_eval:
