9ebc3c27b77ad8a5582e8fc1f96821e7b8175915
Vashisth <vtiwari2@u.rochester.edu>
2024-04-27 15:01:15 -0400

    consistent train and test loader, example=True/False for 1shot

--- lib/data_gsm8k.py
+++ lib/data_gsm8k.py
@@ -24,18 +24,6 @@ def get_examples(split):
     return examples
 
 
-# ANS_RE = re.compile(r"#### (\-?[0-9\.\,]+)")
-# INVALID_ANS = "[invalid]"
-# def extract_answer(completion):
-#     match = ANS_RE.search(completion)
-#     if match:
-#         match_str = match.group(1).strip()
-#         match_str = match_str.replace(",", "")
-#         return match_str
-#     else:
-#         return INVALID_ANS
-# Adjusted regex to capture both the rationale and the answer after '####'
-# Updated regex to capture text before and after '####'
 ANS_RE = re.compile(r"(.*?)####\s*(-?[0-9.,]+)", re.DOTALL)  # Ensure re.DOTALL is used if multiline handling is necessary
 INVALID_ANS = "[invalid]"
 
@@ -53,7 +41,7 @@ def extract_answer(completion):
     return INVALID_ANS  # Return invalid if no pattern matches
 
 
-def get_gsm8k(traindata, testdata, nsamples, seed, seqlen, tokenizer):
+def get_gsm8k(traindata, testdata, nsamples, seed, seqlen, tokenizer, example= True):
     random.seed(seed)
     trainloader = []
     example =  traindata[0]
@@ -64,49 +52,38 @@ def get_gsm8k(traindata, testdata, nsamples, seed, seqlen, tokenizer):
     for _ in range(nsamples): # no need of while true here 
         i = random.randint(1, len(traindata) - 1)
         sample = traindata[i]  # Get the complete sample
-        question_rationale = sample['question']
+        question_instruction = sample['question']
+        if example is True:
+            prepend = f'Example: + {example}\n + Question:'
+            question_instruction = prepend + question_instruction
+        
         rationale, answer = extract_answer(sample['answer'])
-        question_rationale += '\nlets think step by step to get the rationale and the answer:' + rationale
-        rationale_enc = tokenizer(rationale, return_tensors='pt')
-        question_rationale_enc = tokenizer(question_rationale, return_tensors='pt')
-        # if question_rationale_enc.input_ids.shape[1] > seqlen:
-        #     print('skipping sample, too long to encode')
-        #     break
-        answer_enc = tokenizer(str(answer), return_tensors='pt') 
-        # i = random.randint(0, question_rationale_enc.input_ids.shape[1] - seqlen - 1)
-        no_of_rationale_hints = 3 # (rationale + 2 tokens)
-        i = question_rationale_enc.input_ids.shape[1]-rationale_enc.input_ids.shape[1] + no_of_rationale_hints
-        total_len = question_rationale_enc.input_ids.shape[1]
-        j = min(i + seqlen, total_len)
-        inp = question_rationale_enc.input_ids[:, 0:i]
-        # TODO: Ask Lucio what the tar is doing
-        # tar = inp.clone()
-        # tar[:, :-1] = -100
-        # tar[:, :-1] = -(rationale_enc.input_ids.shape[1] +3)
-        tar = question_rationale_enc.input_ids [:, i:]
-        # tar = inp.clone()
-        # tar[:, :-1] = -100
-        # trainloader.append((inp, tar,str(answer)))
-        trainloader.append((inp, tar, str(answer)))
+        # rationale_enc = tokenizer(rationale, return_tensors='pt')
+        # answer_enc = tokenizer(str(answer), return_tensors='pt') 
+        
+        question_instruction += '\nlets think step by step to get the rationale and the answer:'
+        question_instruction_enc = tokenizer(question_instruction, return_tensors='pt')
+                
+        i = question_instruction_enc.input_ids.shape[1]
+        inp = question_instruction_enc.input_ids[:, 0:i]
+        trainloader.append((inp, str(rationale), str(answer)))
 
     testloader = []
     for sample in testdata:
         question = sample['question']
         rationale, answer = extract_answer(sample['answer'])
+        # rationale_en = tokenizer(rationale, return_tensors='pt')
+
+        question_instruction = question + '\n lets think step by step to get the rationale and the answer:\n'
+        if example is True:
+            prepend = f'Example: + {example}\n + Question:'
+            question_instruction = prepend + question_instruction
         
-        # question_rationale = question + '\nRationale: ' + rationale
-        # question_rationale_enc = tokenizer(question_rationale, return_tensors='pt')
-        # padded_question_rationale = tokenizer.pad(question_rationale_enc, max_length=seqlen, padding='max_length', truncation=True)
-        
-        # answer_enc = tokenizer(str(answer), return_tensors='pt')
-        question = question + 'Answer this question:\n'
-        question_en = tokenizer(question, return_tensors='pt')
-        question_en = question_en.input_ids
-        rationale_en = tokenizer(rationale, return_tensors='pt')
+        question_instruction_enc = tokenizer(question, return_tensors='pt')
+        i = question_instruction_enc.input_ids.shape[1]
+        inp = question_instruction_enc.input_ids[:, 0:i]
         rationale_en = rationale_en.input_ids
-        # answer_en = tokenizer(answer, return_tensors='pt')
-        # testloader.append((question_en, rationale_en, str(answer)))
-        testloader.append((question_en, rationale_en, str(answer)))
+        testloader.append((question_instruction_enc, str(rationale_en), str(answer)))
 
     return trainloader, testloader
 
