8e6f59b38edbb95831cb7f433f56648bf77ec658
Amanda Li <xinyue_amanda_li@yahoo.com>
2024-04-27 00:24:52 -0400

    wip: lexical similarity, NEEDS TESTING

--- lib/eval_gsm8k.py
+++ lib/eval_gsm8k.py
@@ -6,7 +6,9 @@ import torch
 import torch.nn as nn
 import pdb
 from .data import get_loaders 
-
+from collections import Counter
+import re
+import string
 
 # TODO: Check why this was done the way it was
 # Now: (borrowing from the lora_ft/evaluate_ppl.py file)
@@ -122,3 +124,86 @@ def eval_ppl_test_gsm8k(model, testenc, bs=1, device=None):
 	torch.cuda.empty_cache()
 
 	return ppl.item()
+
+def normalize_answer(s: str) -> str:
+    def remove_articles(text):
+        return re.sub(r"\b(a|an|the)\b", " ", text)
+
+    def white_space_fix(text):
+        return " ".join(text.split())
+
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        return "".join(ch for ch in text if ch not in exclude)
+
+    def lower(text):
+        return text.lower()
+
+    return white_space_fix(remove_articles(remove_punc(lower(s))))
+
+def f1(prediction, ground_truth, normalize_fn):
+    prediction_tokens = normalize_fn(prediction).split()
+    ground_truth_tokens = normalize_fn(ground_truth).split()
+    common = Counter(prediction_tokens) & Counter(ground_truth_tokens)
+    num_same = sum(common.values())
+
+    if num_same == 0:
+        return 0, 0
+    precision = 1.0 * num_same / len(prediction_tokens)
+    recall = 1.0 * num_same / len(ground_truth_tokens)
+    f1 = (2 * precision * recall) / (precision + recall)
+    return f1
+
+def eval_lexsim_train_gsm8k(model, trainloader, tokenizer, bs=1, device=None):
+	nsamples = len(trainloader)
+
+	# List to store negative log likelihoods
+	f1_sum = 0.0
+	print(f"train lexical similarity: nsamples {nsamples}")
+
+	# Loop through each batch
+	for i in range(0,nsamples,bs):
+		if i % 50 == 0:
+			print(f"sample {i}")
+
+		input_ids = trainloader[i][0].to(device)
+		target_ids = input_ids.clone()
+		target_ids[:, :-1] = -100 #ignore_index token
+		# Calculate negative log likelihood
+		outputs = model(input_ids, labels=target_ids)
+		outputs_decoded = tokenizer.decode(outputs)
+		rationale_decoded = tokenizer.decode(trainloader[i][1].to(device))
+		f1 = f1(outputs_decoded, rationale_decoded, normalize_answer)
+		f1_sum += f1
+
+	# Empty CUDA cache to save memory
+	torch.cuda.empty_cache()
+
+	return f1_sum / nsamples
+
+def eval_lexsim_test_gsm8k(model, testenc, tokenizer, bs=1, device=None):
+	nsamples = len(testenc)
+
+	# List to store negative log likelihoods
+	f1_sum = 0.0
+	print(f"test lexical similarity: nsamples {nsamples}")
+
+	# Loop through each batch
+	for i in range(0,nsamples,bs):
+		if i % 50 == 0:
+			print(f"sample {i}")
+
+		input_ids = testenc[i][0].to(device)
+		target_ids = input_ids.clone()
+		target_ids[:, :-1] = -100 #ignore_index token
+		# Calculate negative log likelihood
+		outputs = model(input_ids, labels=target_ids)
+		outputs_decoded = tokenizer.decode(outputs)
+		rationale_decoded = tokenizer.decode(testenc[i][1].to(device))
+		f1 = f1(outputs_decoded, rationale_decoded, normalize_answer)
+		f1_sum += f1
+
+	# Empty CUDA cache to save memory
+	torch.cuda.empty_cache()
+
+	return f1_sum / nsamples
